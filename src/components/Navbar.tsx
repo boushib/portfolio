622ea67c939cb6da28618d5d8db87adfc76e7189
boushib <boushib01@gmail.com>
2021-02-19 18:22:59 +0100

    create a navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+const Navbar = () => (
+  <nav>
+    <div className="container">
+      <div className="nav-inner">
+        <div className="nav-brand">ElB</div>
+        <ul className="main-menu">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#home">Features</a>
+          </li>
+          <li>
+            <a href="#home">Technologies</a>
+          </li>
+          <li>
+            <a href="#home">Portfolio</a>
+          </li>
+          <li>
+            <a href="#home">Testimonials</a>
+          </li>
+          <li>
+            <a href="#home">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+)
+
+export default Navbar
