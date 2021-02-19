dfe153c2f3e9a3811a8c8f02a8fc84bc10717dce
boushib <boushib01@gmail.com>
2021-02-19 22:06:30 +0100

    add and style the header content

--- src/components/HomePageHeader.tsx
+++ src/components/HomePageHeader.tsx
@@ -4,6 +4,23 @@ import './HomePageHeader.scss'
 const HomePageHeader = () => (
   <header>
     <Navbar />
+    <div className="container">
+      <div className="hero">
+        <h2>👋 El Hassane here</h2>
+        <h1>
+          I’m a Software engineer <br />
+          &amp; Open Source enthusiast
+        </h1>
+        <p>
+          I have over 7 years of experience in the industry, <br />
+          with small, medium & large businesses.
+        </p>
+        <div className="btn-group">
+          <button className="btn">Hire me</button>
+          <button className="btn outlined">Resume</button>
+        </div>
+      </div>
+    </div>
   </header>
 )
 
