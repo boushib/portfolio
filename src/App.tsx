f0783857270747971fcbbd2937116eb0aa1839b9
boushib <boushib01@gmail.com>
2021-03-06 20:50:04 +0100

    fix main app class naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import HomePageHeader from './components/HomePageHeader'
 import './App.scss'
+import WhyChooseMe from './components/WhyChooseMe'
 
 const App = () => (
-  <div className="App">
+  <div className="app">
     <HomePageHeader />
+    <WhyChooseMe />
   </div>
 )
 
---
+++ src/components/WhyChooseMe.tsx
@@ -0,0 +1,7 @@
+const WhyChooseMe: React.FC = () => (
+  <div className="why-choose-me">
+    <h2>Why Choose Me</h2>
+  </div>
+)
+
+export default WhyChooseMe
